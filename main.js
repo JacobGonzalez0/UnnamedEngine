51227d20a8f3d30254565df791fd6f79f560cb0e
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-05-04 19:43:38 -0500

    Added frame counter and frame limiter

--- main.js
+++ main.js
@@ -26,41 +26,109 @@ class Renderer{
     constructor(element){
         this.canvas = document.getElementById(element);
         this.context = this.canvas.getContext("2d");
+
+        return this;
     }
 
-    context(){
+    resize(width, height){
+        this.canvas.width = width;
+        this.canvas.height = height;
+    }
+
+    autoResize(){
+        window.addEventListener('resize', ()=>{
+            this.canvas.width = window.innerWidth;
+            this.canvas.height = window.innerHeight;
+        })
+    }
+
+    get ctx(){
         return this.context;
     }
+
+    set ctx(canvas){
+        this.context = canvas.getContext("2d")
+    }
+
 
 }
 
 class Main{
 
     fps;
+    delta;
 
     constructor(){
-        this.fps = 60
+        this.maxFps = 60;
+        this.fps = 0;
+        this.delta = 0;
+        this.timeStep = 1000 / 60;
+        this.framesThisSecond = 0;
+        this.lastFpsUpdate = 0;
+        this.lastFrameTimeMs = 0;
+        this.debugFlag = false;
+
         this.renderer =  new Renderer("screen");
+        this.renderer.resize(320,200)
 
         requestAnimationFrame(this.loop);
 
-
+        return this;
+    }
+    
+    debug = ()=>{
+        this.debugFlag = true;
+        this.frameCounter =  document.createElement("div");
+        
+        document.body.appendChild(this.frameCounter);
+       
     }
 
-    update(){
+    panic = ()=>{
+        this.delta = 0;
+    }
+
+    update = (delta)=>{
 
     }
 
     draw(){
-
+        if(this.debugFlag){
+            this.frameCounter.innerHTML = "FPS: " + this.fps.toFixed(2);
+        }
+        
     }
 
-    loop = () => {
+    loop = (timestamp) => {
 
-        this.update();
+        if(timestamp < this.lastFrameTimeMs + (1000 / this.maxFps)){
+            requestAnimationFrame(this.loop);
+            return;
+        }
+
+        this.delta += timestamp - this.lastFrameTimeMs;
+        this.lastFrameTimeMs = timestamp;
+
+        if(timestamp > this.lastFpsUpdate + 1000){
+            this.fps = 0.25 * this.framesThisSecond + 0.75 * this.fps;
+
+            this.lastFpsUpdate = timestamp;
+            this.framesThisSecond = 0;
+
+        }
+        this.framesThisSecond++;
+
+        let numUpdateSteps = 0;
+        while(this.delta >= timestamp){
+            this.update(timestamp);
+            this.delta -= timestamp;
+            if(++numUpdateSteps >= 240){
+                this.panic();
+                break;
+            }
+        }
+
         this.draw();
-        
-
         requestAnimationFrame(this.loop)
 
 
@@ -68,4 +136,4 @@ class Main{
 
 }
 
-new Main();
+new Main().debug();
